d16b6db0f1041da96badcd936110d269d2239749
Galinaag4 <Galinaag@yandex.ru>
2022-10-18 22:37:30 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        Product tomato = new Product("Томат",100,10.99);
+        Product banana = new Product("Банан",150,3.58);
+        Product potato = new Product("Картофель",10,30.00);
+        Product orange = new Product("Апельсин",50,5.00);
+        Product tomato1 = new Product("Томат",100,10.99);
+       List<Product> product = new ArrayList<>();
+       product.add(tomato);
+       product.add(banana);
+       product.add(potato);
+       product.add(orange);
+
+
+        System.out.println(Arrays.toString(product.toArray()));
+
+
     }
+
+
 }
+
+
---
+++ src/Product.java
@@ -0,0 +1,68 @@
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+    private int price;
+    private double ves;
+
+    public Product(String name, int price, double ves) {
+        this.name = name;
+        this.price = price;
+        this.ves = ves;
+    }
+
+
+    public void setName(String name) throws IllegalAccessException {
+        if(name==null||name.isEmpty()){
+            throw new IllegalAccessException("Нельзя добавить в корзину");}
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price)throws IllegalAccessException {
+        if (price==0||price<0) {
+            throw new IllegalAccessException("Заполните карточку полностью");}
+            this.price = price;
+        }
+
+
+    public double getVes() {
+        return ves;
+    }
+
+    public void setVes(double ves)throws IllegalAccessException {
+        if (ves==0||ves<0) {
+            throw new IllegalAccessException("Заполните карточку полностью");}
+            this.ves = ves;
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return getPrice() == product.getPrice() && Double.compare(product.getVes(), getVes()) == 0 && Objects.equals(getName(), product.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPrice(), getVes());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", ves=" + ves +
+                '}';
+    }
+}
